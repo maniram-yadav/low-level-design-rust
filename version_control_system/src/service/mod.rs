4e0d0bb013118c36d9187b8ce5deab7ebae53873
maniram <maniramyadav786@gmail.com>
2025-04-03 12:43:15 +0000

    service module code added

--- version_control_system/src/service/mod.rs
+++ version_control_system/src/service/mod.rs
@@ -1,1 +1,10 @@
 mod branch;
+mod commit;
+mod filesnapshot;
+mod repository;
+
+pub use crate::model::branch;
+pub use crate::model::commit;
+pub use crate::model::filesnapshot;
+pub use crate::model::repository;
+pub use crate::model::vcserror;
