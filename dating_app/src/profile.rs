fe5a3594fd1cd0d4bc1655a0c95770ea129db804
maniram <maniramyadav786@gmail.com>
2025-04-05 09:21:27 +0000

    create profile api iplementation

--- dating_app/src/profile.rs
+++ dating_app/src/profile.rs
@@ -1,4 +1,8 @@
 use crate::service::DatingService;
+use crate::input::Input;
+use crate::service::User;
+use uuid::Uuid;
+use crate::service::Gender;
 
 pub struct Profile<'a> {
     service:&'a mut DatingService
@@ -10,5 +14,45 @@ impl<'a> Profile<'a>{
         Self {
             service 
         }
+    }
+
+    pub fn create_profile(&mut self ,current_user : &mut Option<Uuid> ){
+       
+        println!("Create a new profile");
+        print!("Enter Name : ");
+        let name = Input::read();
+        print!("Enter Age : ");
+        let age_str = Input::read();
+
+        let age = match age_str.parse::<u8>() {
+
+            Ok(age) => age,
+            Err(_) => {
+                println!("Invalid agePlease enter a number between 0 to 255");
+                return ;
+            }
+
+        };
+
+        print!("Enter gender (male, Female, Other) : ");
+        let gender_str = Input::read();
+
+        let gender = match gender_str.to_lowercase().as_str() {
+            "male" => Gender::Male,
+            "female" => Gender::Female,
+            "other" => Gender::Other,
+            "any" => Gender::Any,
+            _ => {
+                println!("Invalid gender. Please enter male, female, other , any ");
+                return ;
+            }
+        };
+
+        let new_user = User::new(name,age,gender);
+        self.service.add_user(new_user.clone());
+        *current_user = Some(new_user.id);
+
+        println!("Profile created successfully. Add interests and Preferences");
+        
     }
 }
